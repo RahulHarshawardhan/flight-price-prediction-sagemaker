27abb43d871e91b108db84b0da21ce1c3469b7ad
Rahul Harshawardhan <rahulharshawardhan@yahoo.co.in>
2024-05-28 14:09:43 +0530

    changes made

--- app.py
+++ app.py
@@ -269,8 +269,8 @@ preprocessor = Pipeline(steps=[
 
 
 # Read the training Data
-path = r"C:\Users\Rahul Harshawardhan\Desktop\Flight-Price Prediction Sagemaker\train.csv"
-train = pd.read_csv(path)
+
+train = pd.read_csv("train.csv")
 
 X_train = train.drop(columns= "price")
 y_train = train.price.copy()
